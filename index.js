d48606b1e293876625a14435e8e10b3b7eb5b4f9
AkutoBlade <rared.isaacs@gmail.com>
2022-08-29 10:46:51 +0200

    Update 2

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.get('/:type', (req, res)=> {
   });
 
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Headers", "*");
+    // res.setHeader("Access-Control-Allow-Methods", "*");
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
 });
 
