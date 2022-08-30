484f296aca1e7324d1b15e790dcda17d6cb8c544
AkutoBlade <rared.isaacs@gmail.com>
2022-08-30 16:11:31 +0200

    Update 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cart = require("./routes/routeCart")
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  const allowedOrigins = ["http://192.168.8.193:8080/", "http://localhost:8080/", "http://127.0.0.1:8080"]
+  const allowedOrigins = ["http://192.168.8.193:8080", "http://localhost:8080", "http://127.0.0.1:8080"]
   const origin = req.headers.origin
   if(allowedOrigins.includes(origin)){
   res.setHeader("Access-Control-Allow-Origin",origin);
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     mode: 'no-cors',
-    origin: ["http://192.168.8.193:8080/", "http://localhost:8080/", "http://127.0.0.1:8080"],
+    origin: ["http://192.168.8.193:8080/", "http://localhost:8080", "http://127.0.0.1:8080"],
     credentials: true,
   })
 );
