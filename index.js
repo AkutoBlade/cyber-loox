4acc2e8fd91523a38aa85e762db0a4453bf3acdd
AkutoBlade <rared.isaacs@gmail.com>
2022-08-30 16:22:03 +0200

    Update 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cart = require("./routes/routeCart")
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Origin","http://localhost:8080/");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-*", "*");
