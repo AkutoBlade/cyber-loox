d24b4a9ad41e432081ca9dca568c9daa8bcf3c09
AkutoBlade <rared.isaacs@gmail.com>
2022-08-31 09:19:38 +0200

    Update 3

--- routes/routeUser.js
+++ routes/routeUser.js
@@ -7,20 +7,20 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin","*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-*", "*");
-  next();
-});
-app.use(
-  cors({
-    mode: 'no-cors',
-    origin: ["http://localhost:8080/", "http://127.0.0.1:8080"],
-    credentials: true,
-  })
-);
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin","*");
+//   res.setHeader("Access-Control-Allow-Methods", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-*", "*");
+//   next();
+// });
+// app.use(
+//   cors({
+//     mode: 'no-cors',
+//     origin: ["http://localhost:8080/", "http://127.0.0.1:8080"],
+//     credentials: true,
+//   })
+// );
 //Users
 
 
