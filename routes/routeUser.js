2a77b8a630dc0f9bddc82ea6fc0d8678c017d89a
AkutoBlade <rared.isaacs@gmail.com>
2022-09-08 10:30:41 +0200

    Update Profile

--- routes/routeUser.js
+++ routes/routeUser.js
@@ -130,13 +130,15 @@ router.post('/users/register', bodyParser.json(), async (req, res) => {
     const strQry =
         `UPDATE users
      SET user_fullname = ?, email = ?, phone_number = ?, user_password = ?
-     WHERE user_id = ${req.params.id}`;
+     WHERE user_id = ?`;
      
      
   
-    db.query(strQry, [bd.user_fullname, bd.email, bd.phone_number,  bd.user_password], (err, data) => {
+    db.query(strQry, [bd.user_fullname, bd.email, bd.phone_number,  bd.user_password, bd.user_id], (err, data) => {
         if (err) throw err;
-        res.send(`number of affected record/s: ${data.affectedRows}`);
+        res.json({
+          msg:`Updated User`
+        });
     })
   });
   
